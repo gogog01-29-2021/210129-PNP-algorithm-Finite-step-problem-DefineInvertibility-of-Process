145ee296d21b0e719dced76ba6894dcb4caee3cc
MARSUDTSEAL <78156293+gogog01-29-2021@users.noreply.github.com>
2024-07-08 17:11:57 +0900

    list() a로 b만들기

---
+++ a로 b만들기 .py
@@ -0,0 +1,9 @@
+def solution(before, after):
+    a=sorted(list(before))
+    b=sorted(list(after))
+    print(a,b)
+    return int(a==b)
+
+before,after="string"
+list()    [, , ,]
+.split()   =>gives  .split(" ")
