f85cf131744dd21d11483ca9c93040fea5772b31
MARSUDTSEAL <78156293+gogog01-29-2021@users.noreply.github.com>
2024-12-31 08:05:33 +0900

    Create 3273두수의합시간초과.java

---
+++ 백준/3273두수의합시간초과.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(); //newInteger();
+        int a[]=new int[n];
+        for(int i=0;i<a.length;i++){
+        a[i]=sc.nextInt();
+        }
+        int b=sc.nextInt();
+        int cnt=0;
+        for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]+a[j]==b){
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
