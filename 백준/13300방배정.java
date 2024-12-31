3af42d3b7a6c3488717d6d395b62cdd13d6c6fb0
MARSUDTSEAL <78156293+gogog01-29-2021@users.noreply.github.com>
2024-12-31 17:28:46 +0900

    Create 13300방배정.java

---
+++ 백준/13300방배정.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        HashMap<Integer,Integer> map1=new HashMap<>();
+        for(int i=0;i<n;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            map1.put(a,b);
+        }
+        System.out.println(a,b);
+    }
+}
